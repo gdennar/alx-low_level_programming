e309026a26dfb8abd81d061e49b4f7e80942b670
Golden Dennar <gdennar@yahoo.co.uk>
2022-06-03 15:57:01 -0700

    Bug fix

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * main -Entry point
  * Return: 0
@@ -8,9 +10,8 @@ int main(void)
 int n = 0;
 while (n < 10)
 {
-	printf("%d", n);
+	printf("%d\n", n);
 	n++;
 }
-printf('\n');
 return (0);
 }
