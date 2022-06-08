1878c828155a6856ffa909255b8f19447db3156b
Golden Dennar <gdennar@yahoo.co.uk>
2022-06-08 03:26:47 -0700

    prints lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet -> prints the lowercase alphabets
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+int j = 'a';
+while (j <= 'z')
+{
+	_putchar(j);
+	j++;
+}
+-putchar('\n');
+return (0);
+}
