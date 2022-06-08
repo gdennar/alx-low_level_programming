3d930d05992b8500f3c14eddb35b2ad2213a3aed
Golden Dennar <gdennar@yahoo.co.uk>
2022-06-08 08:45:01 -0700

    hot fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,22 +7,17 @@
  */
 void print_to_98(int n)
 {
-	if (n < 98)
+	while (n < 98)
 	{
-		for (n = 0; n < 98; n++)
-		{
-			printf("%d, ", n);
-		}
+		printf("%d, ", n);
+		n++;
 	}
-	else if (n > 98)
+	while (n > 98)
 	{
-		for (n = 0; n > 98; n--)
-		{
-			printf("%d, ", n);
-		}
+		printf("%d, ", n);
+		n--;
 	}
 	if (n == 98)
-	{
 		printf("%d\n", n);
-	}
+	printf("\n");
 }
