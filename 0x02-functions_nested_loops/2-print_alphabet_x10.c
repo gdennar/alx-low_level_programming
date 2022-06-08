9884ad13ca149c503d09f00caa942b1cf33579d8
Golden Dennar <gdennar@yahoo.co.uk>
2022-06-08 03:47:53 -0700

    prints 10 times the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i = 'a';
+	int j = 0;
+while (a <= 'z')
+{
+	while (j <= 10)
+	{
+		_putchar(i);
+		i++;
+		j++;
+	}
+_putchar('\n');
