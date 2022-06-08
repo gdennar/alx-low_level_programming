ad849ec62f73d24c0c0279b1b04e6dd7fe9cb373
Golden Dennar <gdennar@yahoo.co.uk>
2022-06-08 04:11:15 -0700

    Hot Fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,13 @@ void print_alphabet_x10(void)
 {
 	int i = 'a';
 	int j = 0;
-while (j <= 10)
+while (j < 10)
 {
 	while (i <= 'z')
 	{
 		_putchar(i);
-		i++;
 		j++;
+		i++;
 	}
 _putchar('\n');
 }
