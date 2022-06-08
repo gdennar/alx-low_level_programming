8dfae7f3e8c880076d16fb0f786442bdf2a3e2bb
Golden Dennar <gdennar@yahoo.co.uk>
2022-06-08 08:36:37 -0700

    prints 0 - 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,18 +10,18 @@ void print_to_98(int n)
 	{
 		for (n = 0; n < 98; n++)
 		{
-			_putchar(n);
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ", n);
 		}
 	}
 	else if (n > 98)
 	{
 		for (n = 0; n > 98; n--)
 		{
-			_putchar(n);
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ", n);
 		}
+	}
+	if (n == 98)
+	{
+		printf("%d\n", n);
 	}
 }
