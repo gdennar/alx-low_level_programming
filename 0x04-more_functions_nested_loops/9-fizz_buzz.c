b3cdc80ffd9bca6618be992d688276c35cbe904a
Golden Dennar <gdennar@yahoo.co.uk>
2022-06-09 13:11:41 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,24 +12,24 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("Fizz");
+			printf("FizzBuzz ");
+		}
+		else if
+			(i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz ");
 		}
 		else if
 			(i % 5 == 0 && i % 3 != 0)
-		{
-			printf("Buzz");
-		}
-		else if
-			(i % 3 == 0 && i % 5 == 0)
 			{
-				printf("FizzBuzz");
+				printf("Buzz ");
 			}
 		else if
-			(i == 1)
+			(i == 100)
 			{
-				printf("%d", i);
+				printf("Buzz");
 			}
 		else
 		{
