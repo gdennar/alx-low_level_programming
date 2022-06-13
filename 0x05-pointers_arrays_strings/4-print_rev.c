f4151f4a4b2bc02fc07708b508e9e31973fcc550
Golden Dennar <gdennar@yahoo.co.uk>
2022-06-13 15:08:58 -0700

    Hot fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,14 +9,14 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (*(s + i) != '\0')
+	while ((s + i) != '\0')
 		i++;
 
 	i -= 1;
 
 	while (i >= 0)
 	{
-		_putchar(*s(s + i));
+		_putchar(s(s + i));
 		i--;
 	}
 	_putchar('\n');
