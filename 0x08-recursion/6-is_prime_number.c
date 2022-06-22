9c3c40ff21056eca9d376d22c2b1376f5795de7d
Golden Dennar <gdennar@yahoo.co.uk>
2022-06-22 06:40:54 -0700

    Hot fix

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,12 +5,14 @@
  * @n: int
  * Return: int
  */
+
 int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (getNumPrim(n, n - 1));
+	return (getNumPrim(n, 2));
 }
+
 /**
  * getNumPrim - function that gets if num is 0 or  1
  * @num: input number
@@ -20,10 +22,10 @@ int is_prime_number(int n)
 
 int getNumPrim(int num, int i)
 {
-	if (i == 1)
+	if (num == i)
 	return (1);	
 	
 	if (num % i == 0 && i > 0)
 		return (0);
-		return (getNumPrim(num, i - 1));
+		return (getNumPrim(num, i + 1));
 }
