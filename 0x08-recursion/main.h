9490efff1603c3c338ae8c2077f0ebc1b2c9794a
Golden Dennar <gdennar@yahoo.co.uk>
2022-06-22 06:55:53 -0700

    Returns 1 if a palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,3 +9,5 @@ int is_prime_number(int n);
 int getNumPrim(int num, int i);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int getNumPrim(int num, int i);
+int is_palindrome(char *s);
