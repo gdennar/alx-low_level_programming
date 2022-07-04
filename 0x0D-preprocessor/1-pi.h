c443797e03acb136681f6905ffb9d8fc862768f4
Golden Dennar <gdennar@yahoo.co.uk>
2022-07-04 15:17:23 -0700

    Hot fix

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,3 +1,11 @@
-#ifndef SIZE
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * main - defines a macro names SIZE
+ * Return: 0
+ */
+
 #define SIZE (1024)
-#endif /* #ifndef SIZE */
+
+#endif /* #ifndef */
