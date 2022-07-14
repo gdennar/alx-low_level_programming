6dfe51d194ade34581444e5d1cfa46c8fd98764e
Golden Dennar <gdennar@yahoo.co.uk>
2022-07-14 13:36:59 -0700

    Initial commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_list - function that prints all the elements of a list_t
+ * @list_t: singly list
+ * @h: pointer to singly list
+ * Return: elements of a list_t
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+	else
+		printf("[%d] %s\n", h->len, h->str);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+About Singly linked list
