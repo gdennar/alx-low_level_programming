22dcea432162adcd599daffb34e628944a4845a3
Golden Dennar <gdennar@yahoo.co.uk>
2022-07-26 10:56:19 -0700

    Fixed betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,6 +29,7 @@ int _strlen(char *s)
 int create_file(const char *filename, char *text_content)
 {
 	int fd, fd_write;
+
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
